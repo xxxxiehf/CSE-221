e95a40c7acb813f25caef4536d8ad8e87d199836
qizhyuan <72662836+qizhyuan@users.noreply.github.com>
2021-12-05 01:26:57 -0800

    Update peak-bandwidth-server.cpp
    revise detail

--- network/peak-bandwidth-server.cpp
+++ network/peak-bandwidth-server.cpp
@@ -14,13 +14,12 @@
 #define SERVER_PORT 8889
 #define BUFFER_SIZE 100
 
-int main()
-{
+int main() {
     int fd , client_sock, read_size;
     struct sockaddr_in server, client;
     
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Fail to create socket" << std::endl;
+        std::cerr << "Fail to create socket~" << std::endl;
         close(fd);
     }
      
@@ -28,23 +27,22 @@ int main()
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(SERVER_PORT);
      
-    if(bind(fd,(struct sockaddr *)&server , sizeof(server)) < 0) {
-        std::cerr << "Fail to bind to socket" << std::endl;
+    if(bind(fd,(struct sockaddr *)&server, sizeof(server)) < 0) {
+        std::cerr << "Fail to bind to socket~" << std::endl;
         close(fd);
         exit(EXIT_FAILURE);
     }
      
     listen(fd , 3);
      
-    std::cout << "Waiting for incoming connections..." << std::endl;
+    std::cout << "Waiting...~" << std::endl;
     int l = sizeof(struct sockaddr_in);
     char buffer[BUFFER_SIZE];
     client_sock = accept(fd, (struct sockaddr *)&client, (socklen_t*)&l);
 
-    while (1)
-    {
+    while (1) {
     	if (client_sock < 0) {
-            std::cerr << "Fail to accept to socket" << std::endl;
+            std::cerr << "Fail to accept to socket~" << std::endl;
             close(fd);
             exit(EXIT_FAILURE);
     	}
